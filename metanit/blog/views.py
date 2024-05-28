3778ec34e11557796766215cb198c191b0d86132
killchik <orlovski15555@gmail.com>
2024-05-28 09:21:48 +0300

    Hi

--- metanit/blog/views.py
+++ metanit/blog/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-def index(request):
-    return HttpResponse("<h2>Главная</h2>")
-
+from django.shortcuts import render
 def about(request):
     return HttpResponse("О сайте")
 
 
 def contact(request):
     return HttpResponse("Контакты")
+
+def test(request, pk):
+    return HttpResponse(f"ID = {pk}")
--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from blog import views
+from posts import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('about', views.about),
-    path('contact', views.contact)
+    path('about/', include('blog.urls')),
+    #path('contact/<int:pk>', views.test),
+    path('admin', admin.site.urls),
+    path('posts/', include('posts.urls')),
 ]
